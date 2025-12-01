e331f24765db92ddd04d9328b6df974a80ba8889
ahad <ahadg446@gmail.com>
2025-12-02 00:14:20 +0500

    Improved page loading animation with bubbly effect

--- src/components/LoadingWrapper.tsx
+++ src/components/LoadingWrapper.tsx
@@ -24,7 +24,7 @@ export default function LoadingWrapper({ children }: { children: React.ReactNode
   }, []);
 
   const handleComplete = () => {
-    setShowLoader(true);
+    setShowLoader(false);
   };
 
   return (
